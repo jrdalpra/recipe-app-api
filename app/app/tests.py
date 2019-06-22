011d9d566bbb347ab1e7d3a8ec9cb406bed45986
Jose V Dal Pra Junior <jrdalpra@gmail.com>
2019-06-22 14:43:32 +0100

    first tdd exercise

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,8 +1,13 @@
 from django.test import TestCase
-from app.calc import add
+from app.calc import add, subtract
+
 
 class CalcTests(TestCase):
 
     def test_add(self):
         """Checking if I know how to write a add method"""
-        self.assertEquals(add(1,1), 2)
+        self.assertEquals(add(1, 1), 2)
+
+    def test_subtract_numbers(self):
+        """Checking if I know how to write a add method"""
+        self.assertEquals(subtract(10, 2), 8)
