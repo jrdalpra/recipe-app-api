e2ba8be310badf05dddbb0c3889522ec8bc7b4c0
José Volmei Dal Prá Junior <jv.dalprajunior@aurea.com>
2019-07-01 05:53:14 -0300

    Updated admin to support custom user model

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -24,3 +24,15 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(res, self.regular.name)
         self.assertContains(res, self.regular.email)
+
+    def test_user_change_page(self):
+        url = reverse('admin:core_user_change', args=[self.root.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
